019050dad9a0c7e4b78fe87d57e7c491ab597fb0
Ubuntu <ubuntu@ip-172-31-28-39.ec2.internal>
2017-07-25 18:10:02 +0000

    Add remaining functions to authentication backend from django-aquaint. Remove unused files for templates.

--- users/awsbackend.py
+++ users/awsbackend.py
@@ -1,7 +1,8 @@
 """
-Custom Django Authentication Backend - Taken from: django_warrant/backend.py
+Custom Django Authentication Backend - Taken from: django_warrant/utils.py and backend.py
 https://github.com/MetaMetricsInc/django-warrant
 """
+
 import abc
 
 from boto3.exceptions import Boto3Error
@@ -13,9 +14,41 @@ from django.contrib.auth import get_user_model
 from django.utils.six import iteritems
 
 from warrant import Cognito
-from .utils import cognito_to_dict
 
+### Helper functions for Cognito (utils.py)
+def cognito_to_dict(attr_list,mapping):
+    user_attrs = dict()
+    for i in attr_list:
+        name = mapping.get(i.get('Name'))
+        if name:
+            value = i.get('Value')
+            user_attrs[name] = value
+    return user_attrs
 
+def user_obj_to_django(user_obj):
+    c_attrs = settings.COGNITO_ATTR_MAPPING
+    user_attrs = dict()
+    for k,v in user_obj.__dict__.iteritems():
+        dk = c_attrs.get(k)
+        if dk:
+            user_attrs[dk] = v
+    return user_attrs
+
+def get_cognito(request):
+
+    c = Cognito(settings.COGNITO_USER_POOL_ID,settings.COGNITO_APP_ID,
+                access_token=request.session.get('ACCESS_TOKEN'),
+                id_token=request.session.get('ID_TOKEN'),
+                refresh_token=request.session.get('REFRESH_TOKEN'))
+    changed = c.check_token()
+    if changed:
+        request.session['ACCESS_TOKEN'] = c.access_token
+        request.session['REFRESH_TOKEN'] = c.refresh_token
+        request.session['ID_TOKEN'] = c.id_token
+        request.session.save()
+    return c
+
+### Django authentication backend in Cognito (backend.py)
 class CognitoUser(Cognito):
     user_class = get_user_model()
     # Mapping of Cognito User attribute name to Django User attribute name
--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, {'template_name': 'users/login.html'}, name='login'),
     url(r'^(?P<user>[.-_\w-]+)/$', views.info, name='info')
 ]
